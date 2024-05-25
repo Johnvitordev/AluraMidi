2411fdf5bcd7b1395e26c1a61ec0bac26ec89344
Johnvitordev <joaovitor.dev14@gmail.com>
2024-05-25 20:25:53 -0300

    adicionando as condições no projeto

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+
+    if (elemento && elemento.localName === 'audio') {  
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado');            
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,4 +24,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
